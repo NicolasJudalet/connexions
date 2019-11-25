4d4f7d7174beb791bea670ed7bb068e71e202934
Nicolas Judalet <nicolas.judalet@gmail.com>
2019-11-26 00:16:19 +0100

    :bug: Fix bug with HomeButton link

--- src/components/HomeButton/HomeButton.js
+++ src/components/HomeButton/HomeButton.js
@@ -4,7 +4,7 @@ import whiteHome from "../../../assets/home-white.png"
 import Style from "./HomeButton.style"
 
 export default () => (
-  <Style.Link to="/">
+  <Style.Link to="/home">
     <Style.HomeLogo src={whiteHome} alt="Home" />
   </Style.Link>
 )
