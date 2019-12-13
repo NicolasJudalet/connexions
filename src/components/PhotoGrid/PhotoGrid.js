fbafb4b3445015cc560e44d7606b6e5c8b42e0a4
Nico <nicolas.judalet@gmail.com>
2019-12-13 19:17:06 +0100

    :sparkles: Filter blogPosts based on activated tags

--- src/components/PhotoGrid/PhotoGrid.js
+++ src/components/PhotoGrid/PhotoGrid.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { StaticQuery, graphql } from "gatsby"
+import { intersection, isEmpty, pickBy } from "lodash"
 
 import PhotoComponent from "./PhotoComponent"
 import PdfComponent from "./PdfComponent"
@@ -25,6 +26,9 @@ const query = graphql`
             url
           }
         }
+        tags {
+          id
+        }
       }
     }
     allContentfulAsset {
@@ -47,43 +51,65 @@ const getNodeAsset = (data, assetId) => {
   return assets ? assets[0] : null
 }
 
-const PhotoGrid = () => (
-  <StaticQuery
-    query={query}
-    render={data => (
-      <Style.Wrapper>
-        {data.allContentfulBlogPost.nodes
-          .filter(node => node.photo || node.pdfDescription)
-          .map(node => {
-            const photo = node.photo && getNodeAsset(data, node.photo.id)
-            const pdfUrl = node.pdfDescription && node.pdfDescription.file.url
+const getPostsFilteredByTag = activatedTags => node => {
+  if (
+    activatedTags &&
+    Object.values(activatedTags).every(element => element === false)
+  ) {
+    return true
+  }
 
-            return (
-              <>
-                {photo && (
-                  <Style.Link to={`/${node.slug}`}>
-                    <PhotoComponent
-                      key={node.id}
-                      asset={photo}
-                      date={node.eventDate}
-                    />
-                  </Style.Link>
-                )}
-                {pdfUrl && (
-                  <Style.Link to={`/${node.slug}`}>
-                    <PdfComponent
-                      key={node.id}
-                      pdfUrl={pdfUrl}
-                      date={node.eventDate}
-                    />
-                  </Style.Link>
-                )}
-              </>
-            )
-          })}
-      </Style.Wrapper>
-    )}
-  />
-)
+  const activeFiltersIds = Object.keys(pickBy(activatedTags, Boolean))
+
+  const nodeTagsIds = []
+  node.tags.forEach(tag => {
+    nodeTagsIds.push(tag.id)
+  })
+
+  return !isEmpty(intersection(activeFiltersIds, nodeTagsIds))
+}
+
+const PhotoGrid = ({ activatedTags }) => {
+  console.log(activatedTags)
+  return (
+    <StaticQuery
+      query={query}
+      render={data => (
+        <Style.Wrapper>
+          {data.allContentfulBlogPost.nodes
+            .filter(node => node.photo || node.pdfDescription)
+            .filter(getPostsFilteredByTag(activatedTags))
+            .map(node => {
+              const photo = node.photo && getNodeAsset(data, node.photo.id)
+              const pdfUrl = node.pdfDescription && node.pdfDescription.file.url
+
+              return (
+                <>
+                  {photo && (
+                    <Style.Link to={`/${node.slug}`}>
+                      <PhotoComponent
+                        key={node.id}
+                        asset={photo}
+                        date={node.eventDate}
+                      />
+                    </Style.Link>
+                  )}
+                  {pdfUrl && (
+                    <Style.Link to={`/${node.slug}`}>
+                      <PdfComponent
+                        key={node.id}
+                        pdfUrl={pdfUrl}
+                        date={node.eventDate}
+                      />
+                    </Style.Link>
+                  )}
+                </>
+              )
+            })}
+        </Style.Wrapper>
+      )}
+    />
+  )
+}
 
 export default PhotoGrid
